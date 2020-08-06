1f170cb3ec268454627f83fad082c69825473758
Winston Pemberton <winstonpemberton@gmail.com>
2020-08-06 21:41:08 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,8 +10,13 @@ class StudentsController < ApplicationController
 
 	def create
 	  @student = Student.new
+<<<<<<< HEAD
 	  @student.first_name = params[:student_first_name]
 	  @student.last_name = params[:student_last_name]
+=======
+	  @student.first_name = params[:first_name]
+	  @student.last_name = params[:last_name]
+>>>>>>> 624039f78778a615881010ac7498547682aff0b7
 	  @student.save
 	  redirect_to student_path(@student)
 	end
@@ -22,7 +27,12 @@ class StudentsController < ApplicationController
 
 	def update
 	  @student = Student.find(params[:id])
+<<<<<<< HEAD
 	  @student.update(params.require(:student).permit(:first_name, :last_name))
 	  redirect_to student_path(@student)
+=======
+	  @student.update(params.require(:student).permit(:first_name, :first_name))
+	  redirect_to student_path(@post)
+>>>>>>> 624039f78778a615881010ac7498547682aff0b7
 	end
 end
